379899f174a9a7d47a8fcc7f9872236ae77214e1
frank <frankmwaniki28@gmail.com>
2024-06-10 13:01:52 +0300

    Class1 & install express app

---
+++ college/index.js
@@ -0,0 +1,5 @@
+const express = require('express');
+const app = express();
+app.listen(process.env.port || 4000, function (){
+    console.log('Now listening for requests on:http://localhost:4000');
+});
