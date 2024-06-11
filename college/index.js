ae47d03846e03c97362122de0c51936087797920
frank <frankmwaniki28@gmail.com>
2024-06-11 13:14:00 +0300

    postman&&nodeman install

--- college/index.js
+++ college/index.js
@@ -1,5 +1,7 @@
 const express = require('express');
+const routes = require('./routes/api');
 const app = express();
+app.use(routes);
 app.listen(process.env.port || 4000, function (){
     console.log('Now listening for requests on:http://localhost:4000');
 });
---
+++ college/routes/api.js
@@ -0,0 +1,30 @@
+const express = require('express');
+const routes = express.Router();
+
+
+//get a list of students from the database
+
+routes.get('/students', (req,res)=> {
+    res.send({type:'Get Request'});
+});
+
+
+//add student to the db
+
+routes.post('/students', (req,res)=> {
+    res.send({type:'Post Request '});
+});
+
+//update students in the DB
+
+routes.put('/students/:id', (req,res)=> {
+    res.send({type:'Update Request'});
+});
+
+//delete student to the db
+
+routes.delete('/students/:id', (req,res)=> {
+    res.send({type:'Delete Request'});
+});
+
+module.exports = routes;
