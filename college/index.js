72892b0d2e4f09980b6a2830a751e26a9612a08a
frank <frankmwaniki28@gmail.com>
2024-06-12 13:09:18 +0300

    dotenvIn & schema,model && mongooseIn

--- college/index.js
+++ college/index.js
@@ -1,3 +1,5 @@
+require('dotenv').config();
+require('./helpers/init_mongodb');
 const express = require('express');
 const routes = require('./routes/api');
 const app = express();
@@ -5,3 +7,6 @@ app.use(routes);
 app.listen(process.env.port || 4000, function (){
     console.log('Now listening for requests on:http://localhost:4000');
 });
+
+
+
---
+++ college/models/students.js
@@ -0,0 +1,27 @@
+const mongoose = require('mongoose');
+const schema = mongoose.Schema;
+
+     // Creating Schema
+const studentSchema = new Schema ({
+
+    firstname:{
+        type:String,
+        required: [true,'Firstname is required']
+    },
+
+    
+    lastname:{
+        type:String,
+        required: [true,'Lastname is required']
+    },
+
+    gender:{
+        type:String,
+        
+    }
+
+
+});
+
+const Student = mongoose.model('Student',studentSchema); //create a model that is going to represent our collection in the DB.
+    module.exports = Student; //here we are exporting this file so that we can use it in other files.
